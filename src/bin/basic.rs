4f79f03867215c5bd0804d99598769fcf07292fc
Brian Hinch <brian@tellart.com>
2019-04-11 00:23:59 -0700

    initial commit

---
+++ src/bin/basic.rs
@@ -0,0 +1,67 @@
+extern crate nannou;
+
+use nannou::prelude::*;
+struct Model {}
+fn model(app: &App) -> Model {
+    let _window = app.new_window().with_dimensions(720, 720).build().unwrap();
+    Model {}
+}
+
+fn main() {
+    // nannou::view(view);
+    nannou::app(model, event, view).run();
+}
+
+fn view(app: &App, _model: &Model, frame: Frame) -> Frame {
+// fn view(app: &App, frame: Frame) -> Frame {
+    // Prepare to draw.
+    let draw = app.draw();
+
+    // Clear the background to pink.
+    draw.background().color(LIGHT_PURPLE);
+
+    // Draw a red ellipse with default size and position.
+    draw.ellipse().color(DARK_BLUE);
+
+    // Write to the window frame.
+    draw.to_frame(app, &frame).unwrap();
+
+    // Return the drawn frame.
+    frame
+}
+
+fn event(_app: &App, model: Model, event: Event) -> Model {
+    match event {
+        Event::WindowEvent {
+            simple: Some(event),
+            ..
+        } => match event {
+            Moved(_pos) => {}
+
+            KeyPressed(_key) => {}
+
+            KeyReleased(_key) => {}
+
+            MouseMoved(_pos) => {}
+
+            MouseDragged(_pos, _button) => {}
+
+            MousePressed(_button) => {}
+
+            MouseReleased(_button) => {}
+
+            MouseEntered => {}
+
+            MouseExited => {}
+
+            Resized(_size) => {}
+
+            _other => (),
+        },
+
+        Event::Update(_dt) => {}
+
+        _ => (),
+    }
+    model
+}
